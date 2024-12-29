4d38d4a45a59d2a9f161fd865d2740685bb58d87
Holger Eisold <github@eisold.family>
2024-12-29 09:38:27 +0100

    Added silent mode and protect content
    Signed-off-by: Holger Eisold <github@eisold.family>

--- resources/langs/nl.php
+++ resources/langs/nl.php
@@ -26,4 +26,8 @@ return [
     'full record' => 'volledige opname',
     'Show event location' => 'Toon de plaats van evenementen',
     'Show event date' => 'Toon evenementendatum',
+    'Disable notification sound' => 'Disable notification sound',
+    'If set to \'yes\', users receive a notification but without sound.' => 'If set to \'yes\', users receive a notification but without sound.',
+    'Protect content' => 'Protect content',
+    'If set to \'yes\', message from the bot can\'t be forwarded.' => 'If set to \'yes\', message from the bot can\'t be forwarded.',
 ];
